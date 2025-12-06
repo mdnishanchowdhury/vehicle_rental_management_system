6646a6adb199c697072f177898b173d55ac91b55
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2025-12-06 13:49:12 +0600

    user create and  Authentication has been added

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { userControllerr } from "./user.controller";
 
 const router = express.Router();
-
 router.post('/signup', userControllerr.createUser);
 
 export const userRouter = router;
