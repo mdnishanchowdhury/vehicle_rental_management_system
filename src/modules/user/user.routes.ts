a29c7212974c889eb652d30726ade5d9a94a0c6e
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2025-12-07 21:48:16 +0600

    optimized

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { userControllerr } from "./user.controller";
+import auth from "../../middleware/auth";
 
 const router = express.Router();
+
 router.post("/auth/signup", userControllerr.createUser);
 
-router.get('/users', userControllerr.getAllUsers);
+router.get('/users', auth("admin"), userControllerr.getAllUsers);
 
-router.put("/users/:id", userControllerr.updateUser);
+router.put("/users/:id",auth("admin", "customer"), userControllerr.updateUser);
 
-router.delete("/users/:id", userControllerr.deleteUser);
+router.delete("/users/:id", auth("admin"), userControllerr.deleteUser);
 
 export const userRouter = router;
